1358d2f0f95bb1214c679d43254def3daf936414
Jimmy <amgammarx@hotmail.com>
2023-05-14 21:29:34 +0300

    centered header, started in animation and fixed some body margins

---
+++ general.js
@@ -0,0 +1,28 @@
+let homeWindow = document.querySelector('.home-window');
+let homeWindowBg = document.querySelector('.home-window-bg');
+let homeWindowBgImages = document.querySelectorAll('.home-window-bg img');
+
+// homeWindowBgImages[0].style.top="-300px";
+// homeWindowBgImages[1].style.top="-300px";
+// homeWindowBgImages[4].style.top="110px";
+// homeWindowBgImages[5].style.top="110px";
+
+// homeWindowBgImages[0].style.left="0";
+// homeWindowBgImages[1].style.left="780px";
+// homeWindowBgImages[2].style.left="0";
+// homeWindowBgImages[3].style.left="780px";
+// homeWindowBgImages[4].style.left="0";
+// homeWindowBgImages[5].style.left="780px";
+
+// for (let i = 0; i < homeWindowBgImages.length; i++) {
+    // homeWindowBgImages[i].style.position="relative";
+    // homeWindowBgImages[i].style.top="0";
+    // homeWindowBgImages[i].style.left="0";
+// }
+
+
+// homeWindow.addEventListener('load',function(){
+    // homeWindow.style.position = "relative";
+    // homeWindow.style.top = "0";
+    // homeWindow.style.transition = "all 1s";
+// });
