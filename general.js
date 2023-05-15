d1dff7ec528b0e1c9a84859b7fe42c6e19032cd5
ahmedzaghloul27 <ahmedzaghloul58@gmail.com>
2023-05-15 10:01:14 +0300

    added montras font / added fade on scroll header / adjusted the footer and header with a container div

--- general.js
+++ general.js
@@ -2,3 +2,45 @@ let homeWindow = document.querySelector('.home-window');
 let homeWindowBg = document.querySelector('.home-window-bg');
 let homeWindowBgImages = document.querySelectorAll('.home-window-bg img');
 
+// -------------------------header---------------------------
+(function(){
+
+    var doc = document.documentElement;
+    var w = window;
+  
+    var prevScroll = w.scrollY || doc.scrollTop;
+    var curScroll;
+    var direction = 0;
+    var prevDirection = 0;
+  
+    var header = document.getElementById('header');
+  
+    var checkScroll = function() {
+      curScroll = w.scrollY || doc.scrollTop;
+      if (curScroll > prevScroll) { 
+        //scrolled up
+        direction = 2;
+      }
+      else if (curScroll < prevScroll) { 
+        direction = 1;
+      }
+  
+      if (direction !== prevDirection) {
+        toggleHeader(direction, curScroll);
+      }
+      prevScroll = curScroll;
+    };
+  
+    var toggleHeader = function(direction, curScroll) {
+      if (direction === 2 && curScroll > 126) { 
+        header.classList.add('hide');
+        prevDirection = direction;
+      }
+      else if (direction === 1) {
+        header.classList.remove('hide');
+        prevDirection = direction;
+      }
+    };
+    window.addEventListener('scroll', checkScroll);
+  
+  })();
