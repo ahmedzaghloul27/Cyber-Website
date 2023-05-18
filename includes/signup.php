eae4be2602ad5aa7bb3b69e235f80c0a17a6dc45
Jimmy <amgammarx@hotmail.com>
2023-05-19 01:43:15 +0300

    finally added db but still needs some configuration

--- includes/signup.php
+++ includes/signup.php
@@ -3,29 +3,35 @@
 if(isset($_POST["submit"])){
      $name = $_POST["name"];
      $email = $_POST["email"];
+     $uname = $_POST["uname"];
      $password = $_POST["password"];
+    //  $birthday = $_POST["birthday"];
 
 
      require_once 'dbh.php';
      require_once 'functions.php';
 
     if(emptyInputRegister($name, $email, $password)){
-        header("location: homepage.php?error=emptyinput");
+        header("location: ../login.html?error=emptyinput");
         exit();
     }
     if(!validEmail($email)){
-        header("location: homepage.php?error=invalidemail");
+        header("location: ../login.html?error=invalidemail");
         exit();
     }
     if(emailExist($conn, $email)){
-        header("location: homepage.php?error=emailexist");
+        header("location: ../login.html?error=emailexist");
+        exit();
+    }
+    if(unameExist($conn, $uname)){
+        header("location: ../login.html?error=unameexist");
         exit();
     }
 
-    createUser($conn, $name, $email, $password);
+    createUser($conn, $name, $email, $uname, $password);
 
 } else{
-    header("location: /homepage.php?error=invalidpassword");
+    header("location: ../login.html?error=invalidpassword");
 }
 
 
